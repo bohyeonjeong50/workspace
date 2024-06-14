c5da053d793a91c5db21990f7be23a7943918a34
bohyeonjeong50 <bohyeonjeong50@gmail.com>
2024-06-14 17:28:22 +0900

    1

---
+++ workspace_java/ObjectOriented/src/collection/list/Student.java
@@ -0,0 +1,12 @@
+package collection.list;
+
+public class Student {
+    private String name;
+    private int korScore;
+    private int engScore;
+    private int totalScore;
+
+    public Student(String name, int korScore, int engScore){
+        this.name = name;
+    }
+}
